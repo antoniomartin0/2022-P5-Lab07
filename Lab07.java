cad52f573df8bafaf09c2190e88262b7b57434c2
Antonio Martin <antonio.martin@vtsd.com>
2022-11-30 12:29:13 -0500

    Lab07

---
+++ Lab07.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.applet.*;
+public class Lab07 extends Applet
+{
+    public void paint(Graphics g)
+    {
+        // DRAW BACKGROUND
+
+        g.drawRect(0,);
+
+
+    }
+
+}
